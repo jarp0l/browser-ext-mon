ec9ee8739c266bbcb7f2e5a343b0f5e6cb6baadc
jarp0l <57973356+jarp0l@users.noreply.github.com>
2024-01-26 00:35:01 +0545

    feat(fe): init basic streamlit project + docker & devcontainer

---
+++ frontend/streamlit-app/streamlit_app/dashboard.py
@@ -0,0 +1,12 @@
+import streamlit as st
+
+
+def main():
+    st.set_page_config(page_title="Dashboard")
+    st.header("Dashboard")
+    
+    st.write("Hello World!")
+
+
+if __name__ == "__main__":
+    main()
---
+++ frontend/streamlit-app/streamlit_app/pages/1_agents.py
@@ -0,0 +1,6 @@
+import streamlit as st
+
+st.set_page_config(page_title="Agents")
+st.header("Agents")
+
+st.write("Download agents from the list below")
