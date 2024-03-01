3da2b91a63d5e2b55300293c74b26a3232da86a1
jarp0l <57973356+jarp0l@users.noreply.github.com>
2024-03-01 09:45:36 +0545

    feat: add structs for osquery requests/responses

--- backend/pb/osquery/config.go
+++ backend/pb/osquery/config.go
@@ -6,6 +6,20 @@ import (
 	"github.com/labstack/echo/v5"
 )
 
+type ConfigRequest struct {
+	NodeKey string `json:"node_key"`
+}
+
+type ConfigResponse struct {
+	Schedule    *map[string]ScheduleItem `json:"schedule,omitempty"`
+	NodeInvalid bool                     `json:"node_invalid"`
+}
+
+type ScheduleItem struct {
+	Query    string `json:"query"`
+	Interval int    `json:"interval"`
+}
+
 func GetConfig(c echo.Context) error {
 	return c.JSON(http.StatusOK, map[string]interface{}{
 		"config": map[string]interface{}{
--- backend/pb/osquery/enroll.go
+++ backend/pb/osquery/enroll.go
@@ -6,13 +6,23 @@ import (
 	"github.com/labstack/echo/v5"
 )
 
-type EnrollNode struct {
-	NodeKey string `json:"node_key"`
+type EnrollRequest struct {
+	APIKey       string `json:"api_key"`
+	OwnerEmail   string `json:"owner_email"`
+}
+
+type EnrollResponse struct {
+	NodeKey     string `json:"node_key"`	// TODO: if API key is valid, hash and salt org_id and return as node_key
+	NodeInvalid bool   `json:"node_invalid"`
 }
 
 func Enroll(c echo.Context) error {
-	enrollNode := &EnrollNode{
+	enrollRes := &EnrollResponse{
 		NodeKey: "this_is_node_key",
 	}
-	return c.JSON(http.StatusCreated, enrollNode)
+	enrollReq := new(EnrollRequest)
+	if err := c.Bind(enrollReq); err != nil {
+		return c.String(http.StatusBadRequest, err.Error())
+	}
+	return c.JSON(http.StatusCreated, enrollRes)
 }
