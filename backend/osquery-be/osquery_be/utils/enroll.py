ab4b8cd4140087374a8c28a52f8d6f111e61d504
jarp0l <57973356+jarp0l@users.noreply.github.com>
2024-03-07 01:07:19 +0545

    refactor: improve code documentation, structure and use config file

--- backend/osquery-be/osquery_be/utils/enroll.py
+++ backend/osquery-be/osquery_be/utils/enroll.py
@@ -1,26 +1,21 @@
 import json
 import logging
-import os
 
 import httpx
-
-from osquery_be.schemas import EnrollRequest, EnrollResponse
+from osquery_be.config import settings
+from osquery_be.schemas import EnrollRequest, EnrollResponse, EnrollSecret
 from osquery_be.utils.exceptions import InvalidAPIKeyException
 
-# host.docker.internal is a special DNS name which resolves to the internal IP address used by the host
-# We need this to connect to backen/pb instance running on the host machine
-PB_API_URL = os.getenv("PB_API_DOCKER_URL", "http://localhost:8090/api")
-SERVICE_TOKEN = os.getenv("PB_OSQUERY_SERVICE_TOKEN", "osquery-be")
 
-
-def enroll_node(enroll_request: EnrollRequest):
-    enroll_secret = json.loads(enroll_request.enroll_secret)
-    api_key = enroll_secret["api_key"]
-    owner_email = enroll_secret["owner_email"]
-    host_identifier = enroll_request.host_identifier
+def enroll_node(enroll_req: EnrollRequest):
+    enroll_secret = json.loads(enroll_req.enroll_secret)
+    enroll_secret_model = EnrollSecret(**enroll_secret)
+    api_key = enroll_secret_model.api_key
+    owner_email = enroll_secret_model.owner_email
+    host_identifier = enroll_req.host_identifier
 
     headers = {
-        "X-SERVICE-TOKEN": SERVICE_TOKEN,
+        "X-SERVICE-TOKEN": settings.service_token,
     }
 
     try:
@@ -28,7 +23,7 @@ def enroll_node(enroll_request: EnrollRequest):
 
         # Check if the api_key is valid
         search_orgs = client.get(
-            f"{PB_API_URL}/collections/organizations/records",
+            f"{settings.pb_api_url}/collections/organizations/records",
             headers=headers,
             params={"filter": f"(api_key='{api_key}')"},
         ).json()
@@ -39,7 +34,7 @@ def enroll_node(enroll_request: EnrollRequest):
 
         # Check if the node is already enrolled
         search_nodes = client.get(
-            f"{PB_API_URL}/collections/nodes/records",
+            f"{settings.pb_api_url}/collections/nodes/records",
             headers=headers,
             params={"filter": f"(uuid='{host_identifier}')"},
         ).json()
@@ -48,7 +43,7 @@ def enroll_node(enroll_request: EnrollRequest):
         if search_nodes["totalItems"] != 1:
             # Create a new node since there is no node with the given host_identifier/uuid
             _ = client.post(
-                f"{PB_API_URL}/collections/nodes/records",
+                f"{settings.pb_api_url}/collections/nodes/records",
                 headers=headers,
                 json={
                     "uuid": host_identifier,
@@ -59,10 +54,15 @@ def enroll_node(enroll_request: EnrollRequest):
         return EnrollResponse(node_key=host_identifier)
 
     except InvalidAPIKeyException:
-        logging.exception("Invalid API Key")
-        return EnrollResponse(node_key="")
+        logging.debug("Invalid API Key")
+        return EnrollResponse(node_invalid=True)
+
+    # If the backend/pb is not running, httpx.ConnectError will be raised
+    except httpx.ConnectError:
+        logging.error("Is the backend/pb running?")
+        return EnrollResponse(node_invalid=True)
 
     except Exception as exc:
         logging.exception("Unexpected error during enrollment")
         logging.exception(exc)
-        return EnrollResponse(node_key="")
+        return EnrollResponse(node_invalid=True)
