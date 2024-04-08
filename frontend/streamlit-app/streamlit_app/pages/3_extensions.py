0b9819b7cd37a66780dc40d2a096f1fe870e3b37
jarp0l <57973356+jarp0l@users.noreply.github.com>
2024-04-08 21:57:10 +0545

    refactor: extensions view

--- frontend/streamlit-app/streamlit_app/pages/3_extensions.py
+++ frontend/streamlit-app/streamlit_app/pages/3_extensions.py
@@ -1,57 +1,111 @@
 import pandas as pd
 import streamlit as st
 
-from streamlit_app.utils.apis import get_extensions, get_nodes
+from streamlit_app.utils.apis import get_extensions, get_nodes, get_organization
 from streamlit_app.utils.auth_form import LoginForm, auth_form
 
 
 def populate_page():
-    st.subheader("List of firefox extensions")
-    org_id = "72b53p7ai8s9wdm"
-    data_node = get_nodes(org_id)
+    if "bem-email" not in st.session_state:
+        return
+    admin_email = st.session_state["bem-email"]
 
-    item_node = data_node["items"]
+    org_data = get_organization(admin_email)
+    org_id = org_data["items"][0].get("id")
+    if org_id is None:
+        st.error("Error fetching organization info!")
+        return
 
-    for item in item_node:
-        extension_firefox_node = get_extensions("firefox_extensions", item.get("id"))
-        if extension_firefox_node is None:
-            st.error("error fetching extension records")
+    nodes = get_nodes(org_id)
+    if nodes is None:
+        st.error("Error fetching nodes!")
+        return
+
+    all_firefox_extensions = []
+    all_chrome_extensions = []
+    for node in nodes["items"]:
+        firefox_extensions = get_extensions("firefox_extensions", node.get("id"))
+        chrome_extensions = get_extensions("chrome_extensions", node.get("id"))
+        if firefox_extensions is None:
+            st.error("Error fetching firefox extensions!")
             return
-        if extension_firefox_node["totalItems"]:
-            df = pd.DataFrame(extension_firefox_node["items"])
-            st.data_editor(df)
-        else:
-            st.error("total items in node is zero")
-
-        total_item1 = extension_firefox_node["totalItems"]
-
-    st.subheader("list of chrome extensions")
-    for item in item_node:
-        extension_chrome_node = get_extensions("chrome_extensions", item.get("id"))
-        if extension_chrome_node is None:
-            st.error("error fetching extension records")
+        if chrome_extensions is None:
+            st.error("Error fetching chrome extensions!")
             return
-        if extension_chrome_node["totalItems"]:
-            df = pd.DataFrame(extension_chrome_node["items"])
-            st.data_editor(df)
-        else:
-            st.error("total items in node is zero")
+        all_firefox_extensions.extend(firefox_extensions["items"])
+        all_chrome_extensions.extend(chrome_extensions["items"])
 
-        total_item2 = extension_chrome_node["totalItems"]
+        firefox_df = pd.DataFrame(all_firefox_extensions)
+        chrome_df = pd.DataFrame(all_chrome_extensions)
 
-    all_extensions = df["identifier"].value_counts().reset_index()
-    all_extensions.columns = ["identifier", "count"]
-    top_5_extensions = all_extensions.head(5)
+    with st.container(border=True):
+        st.subheader("Firefox Extensions")
+        st.data_editor(
+            firefox_df,
+            column_order=(
+                "name",
+                "version",
+                "identifier",
+                "description",
+                "creator",
+                "node_id",
+                "active",
+                "disabled",
+                "path",
+            ),
+            column_config={
+                "active": st.column_config.CheckboxColumn("Enabled?", disabled=True),
+                "disabled": st.column_config.CheckboxColumn("Disabled?", disabled=True),
+                "creator": "Creator",
+                "description": "Description",
+                "identifier": "Identifier",
+                "name": "Name",
+                "node_id": "Node ID",
+                "path": "Path",
+                "uid": "UID",
+                "version": "Version",
+            },
+        )
 
-    # Display the top 5 extensions in a bar chart
-    st.subheader("Top 5 Most Used Extensions")
-    st.bar_chart(top_5_extensions.set_index("identifier"))
-    total_extensions = total_item1 + total_item2
-    st.write(total_extensions)
+    with st.container(border=True):
+        st.subheader("Chrome Extensions")
+        st.data_editor(
+            chrome_df,
+            column_order=(
+                "name",
+                "version",
+                "referenced_identifier",
+                "description",
+                "author",
+                "browser_type",
+                "node_id",
+                "permissions",
+                "optional_permissions",
+                "manifest_hash",
+                "profile_path",
+                "from_webstore",
+            ),
+            column_config={
+                "name": "Name",
+                "version": "Version",
+                "referenced_identifier": "Identifier",
+                "description": "Description",
+                "author": "Author",
+                "browser_type": "Browser Type",
+                "node_id": "Node ID",
+                "permissions": "Permissions",
+                "optional_permissions": "Optional Permissions",
+                "manifest_hash": "Manifest Hash",
+                "profile_path": "Profile Path",
+                "from_webstore": st.column_config.CheckboxColumn(
+                    "From Webstore?", disabled=True
+                ),
+            },
+        )
 
 
 def build_page():
-    st.set_page_config(page_title="Extensions")
+    st.set_page_config(page_title="Extensions", layout="wide")
     st.header("Extensions")
 
     if st.sidebar.button("Logout"):
