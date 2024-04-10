15855aa6605f00b823913db425653be56638989a
Prajwol Pradhan <57973356+jarp0l@users.noreply.github.com>
2024-04-10 14:32:50 +0545

    feat: replace st.data_editor with aggrid for better tables

--- frontend/streamlit-app/streamlit_app/pages/3_extensions.py
+++ frontend/streamlit-app/streamlit_app/pages/3_extensions.py
@@ -1,5 +1,6 @@
 import pandas as pd
 import streamlit as st
+from st_aggrid import AgGrid
 
 from streamlit_app.utils.apis import get_extensions, get_nodes, get_organization
 from streamlit_app.utils.auth_form import LoginForm, auth_form
@@ -47,80 +48,141 @@ def populate_page():
             [
                 extension
                 for extension in all_chrome_extensions
-                if extension.get("browser_type") == "brave"
+                if extension.get("browser_type") == "braver"
             ]
         )
+
+    grid_options = {
+        "autoSizeStrategy": {"type": "fitCellContents"},
+        "rowSelection": "single",
+        "defaultColDef": {
+            "filter": "agTextColumnFilter",
+            "floatingFilter": True,
+            "cellStyle": {"line-height": "32px"},
+        },
+        "rowHeight": 36,
+        "pagination": True,
+        "paginationPageSize": 10,
+        "paginationPageSizeSelector": [10, 25, 50],
+        "alwaysShowHorizontalScroll": True,
+        "alwaysShowVerticalScroll": True,
+    }
+
+    # alternative to dynamically adjust tabs
+    # problem is the tab indexing has to be adopted as per this approach
+    # st.tabs(
+    #     [
+    #         name
+    #         for (name, df) in [
+    #             ("Firefox", firefox_df),
+    #             ("Chrome", chrome_df),
+    #             ("Brave", brave_df),
+    #         ]
+    #         if not df.empty
+    #     ]
+    # )
 
     tabs = st.tabs(["Firefox", "Chrome", "Brave"])
     with tabs[0]:
         st.subheader("Firefox Extensions")
-        st.data_editor(
-            firefox_df,
-            column_order=(
-                "name",
-                "version",
-                "identifier",
-                "description",
-                "creator",
-                "node_id",
-                "active",
-                "disabled",
-                "path",
-            ),
-            column_config={
-                "active": st.column_config.CheckboxColumn("Enabled?", disabled=True),
-                "disabled": st.column_config.CheckboxColumn("Disabled?", disabled=True),
-                "creator": "Creator",
-                "description": "Description",
-                "identifier": "Identifier",
-                "name": "Name",
-                "node_id": "Node ID",
-                "path": "Path",
-                "uid": "UID",
-                "version": "Version",
-            },
+
+        firefox_grid_options = {
+            "columnDefs": [
+                {
+                    "field": "name",
+                    "cellRenderer": "agGroupCellRenderer",
+                    "checkboxSelection": True,
+                },
+                {"field": "version"},
+                {"field": "identifier"},
+                {"field": "description"},
+                {"field": "creator"},
+                {"field": "node_id"},
+                {"field": "active"},
+                {"field": "disabled"},
+                {"field": "path"},
+            ],
+            "rowData": firefox_df.to_dict("records"),
+        }
+        grid_options.update(firefox_grid_options)
+        firefox_grid = AgGrid(
+            None,
+            gridOptions=grid_options,
+            allow_unsafe_jscode=True,
+            key="firefox_grid",
+            theme="streamlit",
         )
 
-    def get_data_editor_for_browser(browser_df):
-        return st.data_editor(
-            browser_df,
-            column_order=(
-                "name",
-                "version",
-                "identifier",
-                "description",
-                "author",
-                "node_id",
-                "permissions",
-                "optional_permissions",
-                "manifest_hash",
-                "profile_path",
-                "from_webstore",
-            ),
-            column_config={
-                "name": "Name",
-                "version": "Version",
-                "identifier": "Identifier",
-                "description": "Description",
-                "author": "Author",
-                "node_id": "Node ID",
-                "permissions": "Permissions",
-                "optional_permissions": "Optional Permissions",
-                "manifest_hash": "Manifest Hash",
-                "profile_path": "Profile Path",
-                "from_webstore": st.column_config.CheckboxColumn(
-                    "From Webstore?", disabled=True
-                ),
-            },
-        )
+    def chrome_grid_options(browser_type):
+        return {
+            "columnDefs": [
+                {
+                    "field": "name",
+                    "cellRenderer": "agGroupCellRenderer",
+                    "checkboxSelection": True,
+                },
+                {"field": "version"},
+                {"field": "identifier"},
+                {"field": "description"},
+                {"field": "author"},
+                {"field": "node_id"},
+                {"field": "permissions"},
+                {"field": "optional_permissions"},
+                {"field": "profile_path"},
+            ],
+            "rowData": browser_type.to_dict("records"),
+        }
 
     with tabs[1]:
         st.subheader("Chrome Extensions")
-        get_data_editor_for_browser(chrome_df)
+
+        grid_options.update(chrome_grid_options(chrome_df))
+        chrome_grid = AgGrid(
+            None,
+            gridOptions=grid_options,
+            allow_unsafe_jscode=True,
+            key="chrome_grid",
+            theme="streamlit",
+        )
 
     with tabs[2]:
         st.subheader("Brave Extensions")
-        get_data_editor_for_browser(brave_df)
+
+        grid_options.update(chrome_grid_options(brave_df))
+        brave_grid = AgGrid(
+            None,
+            gridOptions=grid_options,
+            allow_unsafe_jscode=True,
+            key="brave_grid",
+            theme="streamlit",
+        )
+
+    try:
+        selected_row = (
+            firefox_grid.selected_rows_id
+            or chrome_grid.selected_rows_id
+            or brave_grid.selected_rows_id
+        )
+        with st.container(border=True):
+            st.subheader("Scan Results")
+
+            if selected_row and firefox_grid.selected_rows_id:
+                selected_extension = pd.json_normalize(
+                    firefox_df.iloc[selected_row].to_dict(orient="records")
+                )
+                st.write("Firefox Extension: " + selected_extension["name"][0])
+
+            if selected_row and brave_grid.selected_rows_id:
+                selected_extension = brave_df.iloc[selected_row]
+                "Brave Extension"
+                selected_extension
+
+            if selected_row and chrome_grid.selected_rows_id:
+                selected_extension = chrome_df.iloc[selected_row]
+                "Chrome Extension"
+                selected_extension
+    except Exception:
+        pass
 
 
 def build_page():
